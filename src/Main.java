3f39756170a7479b8dde71f01608a12952d9a3b7
duisheevvva <duisheeva.aizat@icloud.com>
2025-12-16 12:10:13 +0600

    added tasks for students

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
+//        Muhammad  -  create package model,
+//        Samagan   -  create dao,service,db,generateId packages and createCustomer method
+//        Elmirbek  -  getAllCustomer, getById  methods
+//        Nursultan -  update,delete methods
+//        Aidana    -  sortByDateOfBirth method
+//        Adina     -  filterByGender
+          System.out.println("Test");
     }
 }
